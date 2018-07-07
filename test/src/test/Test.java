ffb9a461e9d6da94dba698d450f7f04ce84a528f
Gautam <Gautam@DESKTOP-7JA8S6R>
2018-07-07 20:18:19 +0530

    initial commit

---
+++ test/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test {
+   
+	public static void main(String s[]) {
+		
+		System.out.println("HELLO WORLD");
+		System.out.println("do something");
+		System.out.println("hellow");
+	}
+}
